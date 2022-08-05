c3ac6941f5a9ec853f2b8f5e7472f5b88874bfe6
joserb10 <joserb10>
2022-08-05 08:59:10 -0500

    ejercicio2 v2.0

--- ejercicio2/script.js
+++ ejercicio2/script.js
@@ -94,25 +94,36 @@ let json = {
             }
         }
 
+
+
+//Crear copia del objeto json.data sin alterar el original        
 let result = {...json.data};
+
+//Iterar por cada llave del objeto result
 Object.keys(result).forEach(element => {
+
+    //Ordenar cada llave por el campo limit de mayor a menor
     result[element].sort((a, b) => {
         return b.limit - a.limit ;
     });
 
+    //Filtrar y obtener solo el registro mayor
     result[element] = result[element].filter((service, index) => {
         return index === 0;
     })[0];
 
+    //Añadir los campos over y under con los id correspondientes
     result[element]['over'] = values[result[element]['over_carrier_service_id']];
     result[element]['under'] = values[result[element]['under_carrier_service_id']];
-    console.log(result[element]['over'])
-    console.log(result[element]['under'])
-    console.log(Object.keys(values).find(val => val == result[element]['over_carrier_service_id']));
 
-    console.log(result[element]);
+    //Eliminar las llaves over_carrier_service_id y under_carrier_service_id
+    delete result[element]['over_carrier_service_id'];
+    delete result[element]['under_carrier_service_id'];
 });
+
 console.log(result);
 
-
+//Mostrar el jsons en el html 
 document.getElementById("result").innerHTML = JSON.stringify(result, null, 4);
+document.getElementById("json").innerHTML = JSON.stringify(json, null, 4);
+document.getElementById("values").innerHTML = JSON.stringify(values, null, 4);
